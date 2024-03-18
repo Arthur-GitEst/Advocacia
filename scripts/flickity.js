c306e42913a5bec09ccfb75ba3c16413fd5d2460
Arthur de Barros <163930420+Arthur-GitEst@users.noreply.github.com>
2024-03-18 19:03:06 -0300

    Add files via upload

---
+++ scripts/flickity.js
@@ -0,0 +1,11 @@
+var flkty = new Flickity( '.main-carousel', {
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  draggable: false,
+  imagesLoaded: true,
+  lazyLoad: true,
+  // options
+});
+
+
